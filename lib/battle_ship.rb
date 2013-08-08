26909d3bdc19757f64e411f3595ec675cf0f8baf
DavidRagone <dmragone@gmail.com>
2013-08-08 07:10:15 -0700

    First few tests (failing)

--- lib/battle_ship.rb
+++ lib/battle_ship.rb
@@ -1,5 +1,9 @@
 require "battle_ship/version"
 
 module BattleShip
-  # Your code goes here...
+  class << self
+    def read(namespace, uid)
+      value = Rails.cache.read(namespace.to_s << '_' << uid.to_s)
+    end
+  end
 end
--- spec/lib/battle_ship_spec.rb
+++ spec/lib/battle_ship_spec.rb
@@ -1,7 +1,65 @@
 require 'spec_helper'
 
+class Rails
+  def self.cache
+    Cache
+  end
+end
+
+class Cache
+  def read(value)
+  end
+end
+
 describe BattleShip do
-  it "should exist" do
-    described_class.new.should_not be_nil
+  before do
+    # setup
+  end
+
+  describe ".read" do
+    shared_examples_for :read do
+      let(:cache_double) { double }
+
+      it "retrieves value from Rails.cache.read" do
+        Rails.should_receive(:cache) { cache_double }
+        cache_double.should_receive(:read).with("namespace_#{uniqueid}")
+
+        BattleShip.read(:namespace, uniqueid)
+      end
+
+      it "returns value from Rails.cache.read" do
+        Rails.stub_chain(:cache, :read) { 'a value' }
+
+        BattleShip.read(:foo, :bar).should eq 'a value'
+      end
+
+      it "increments namespace counter when value is non-nil" do
+        incrementer = double increment: 5
+        # How stub same object with two differen tmethods, expect both be called?
+        Rails.stub(:cache) { double :cache, read: 'a value' }
+        Rails.stub(:cache) { incrementer }
+        incrementer.should_receive(:increment)
+
+        BattleShip.read(:foo, :bar)
+      end
+    end
+
+    context 'when uniqueid is a symbol' do
+      let(:uniqueid) { :foo }
+
+      it_behaves_like :read
+    end
+
+    context 'when uniqueid is a string' do
+      let(:uniqueid) { 'foo' }
+
+      it_behaves_like :read
+    end
+
+    context 'when uniqueid is a number' do
+      let(:uniqueid) { 4 }
+
+      it_behaves_like :read
+    end
   end
 end
