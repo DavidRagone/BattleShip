86930f8a9d564a835ed9b8b67d1d3dd3315e50c0
DavidRagone <dmragone@gmail.com>
2013-08-08 22:37:24 -0700

    Replace Rails.cache with cache method

--- lib/battle_ship.rb
+++ lib/battle_ship.rb
@@ -3,22 +3,26 @@ require "battle_ship/version"
 module BattleShip
   class << self
     def read(namespace, uid)
-      value = Rails.cache.read(namespaced(namespace, uid))
+      value = cache.read(namespaced(namespace, uid))
       increment("#{namespace}_#{value.nil? ? 'miss' : 'hit'}")
       value
     end
 
     def write(namespace, uid, value, options = {})
-      Rails.cache.write(namespaced(namespace, uid), value, options)
+      cache.write(namespaced(namespace, uid), value, options)
     end
 
     private
     def increment(key, amount = 1, options = {})
-      Rails.cache.increment(key, amount, options)
+      cache.increment(key, amount, options)
     end
 
     def namespaced(namespace, uid)
       namespace.to_s << '_' << uid.to_s
+    end
+
+    def cache
+      Rails.cache
     end
   end
 end
